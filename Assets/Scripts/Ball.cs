ec10b7fcf5c10ead3ea6f3134f89ecbd31165fef
Nicholas <nalesse@student.fitchburgstate.edu>
2021-04-13 19:44:32 -0400

    Implemented Key and obstacle Logic

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField] private bool hasKey = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Key"))
+        {
+            hasKey = true;
+            Destroy(other.gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (hasKey)
+        {
+            if (collision.collider.CompareTag("Obsticle"))
+            {
+                Destroy(collision.gameObject);
+                hasKey = false;
+            }
+        }
+    }
+}
