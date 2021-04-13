85e95b153cf911d7d7fb2cf174f530d36ee612ec
Nicholas <nalesse@student.fitchburgstate.edu>
2021-04-12 21:33:51 -0400

    Built prototype Maze and got rotation controls working
    - Built a simple maze prototype and got rotation
    - could not figure out how to lock rotation

---
+++ Assets/Scripts/Maze.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Maze : MonoBehaviour
+{
+    [SerializeField] private float rotationSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float verticleInput = Input.GetAxis("Vertical");
+        float horizontalInput = Input.GetAxis("Horizontal");
+        Vector3 forward = new Vector3(1, 0, 0);
+        Vector3 left = new Vector3(0, 0, -1);
+
+        transform.Rotate(forward * Time.deltaTime * rotationSpeed * verticleInput);
+        transform.Rotate(left * Time.deltaTime * rotationSpeed * horizontalInput);
+
+        
+    }
+}
