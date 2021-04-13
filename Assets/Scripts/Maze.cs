420debff2426bf2a056220c84bc8021dcdc8fa68
Nicholas <nalesse@student.fitchburgstate.edu>
2021-04-12 22:50:23 -0400

    Found a solution to be able to lock y axis

--- Assets/Scripts/Maze.cs
+++ Assets/Scripts/Maze.cs
@@ -5,23 +5,31 @@ using UnityEngine;
 public class Maze : MonoBehaviour
 {
     [SerializeField] private float rotationSpeed;
+    [SerializeField] float verticleInput;
+    [SerializeField] float horizontalInput;
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.Rotate(0, 5, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        float verticleInput = Input.GetAxis("Vertical");
-        float horizontalInput = Input.GetAxis("Horizontal");
+        verticleInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+        
         Vector3 forward = new Vector3(1, 0, 0);
         Vector3 left = new Vector3(0, 0, -1);
+        Vector3 up = new Vector3(0, 1, 0);
 
         transform.Rotate(forward * Time.deltaTime * rotationSpeed * verticleInput);
         transform.Rotate(left * Time.deltaTime * rotationSpeed * horizontalInput);
 
-        
+        Vector3 myRotation = transform.rotation.eulerAngles;
+        myRotation.y = 0;
+        transform.rotation = Quaternion.Euler(myRotation);
+
+
     }
 }
