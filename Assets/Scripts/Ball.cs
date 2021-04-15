67c64db9280f616fba319c4e22877fb4edd30724
Nicholas <nalesse@student.fitchburgstate.edu>
2021-04-15 14:24:28 -0400

    Implemented portal System

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,14 +1,16 @@
 ﻿using System.Collections;
+using System.Linq;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
     [SerializeField] private bool hasKey = false;
+    List<GameObject> portals = new List<GameObject>();
     // Start is called before the first frame update
     void Start()
     {
-        
+        portals = GameObject.FindGameObjectsWithTag("Portal").ToList();
     }
 
     // Update is called once per frame
@@ -28,12 +30,31 @@ public class Ball : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
+        // Destroys the obstacle if the key has been collected and the ball hits the obstacle  
         if (hasKey)
         {
             if (collision.collider.CompareTag("Obsticle"))
             {
                 Destroy(collision.gameObject);
                 hasKey = false;
+            }
+        }
+
+        if (collision.collider.CompareTag("Portal"))
+        {
+            // Gets the index of the portal that the ball collided with then sends the ball to the other portal
+
+            int index = portals.IndexOf(collision.gameObject);
+            Vector3 offset = new Vector3(2, 0, 0);
+            
+            switch(index)
+            {
+                case 0:
+                    transform.position = portals[index + 1].transform.position + offset;
+                    break;
+                case 1:
+                    transform.position = portals[index - 1].transform.position + offset;
+                    break;
             }
         }
     }
